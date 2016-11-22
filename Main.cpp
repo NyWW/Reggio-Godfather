f4e2e4434990250d2c86bfd5151704a1040cee16
NyWW <wzy422702500@gmail.com>
2016-11-21 19:32:14 -0500

    New Stuff

--- Main.cpp
+++ Main.cpp
@@ -9,3 +9,37 @@
 #include <iomanip>
 #include <fstream>
 using namespace std;
+
+class Godfather{
+public:
+    struct Plane{
+        char Data;
+        int Fuel;
+        int PPL;
+        int Cargo;
+        bool GC;
+    };
+
+
+
+private:
+    int RelativeTime;
+    double AvgTOWaitTime;
+    double AvgLDWaitTime;
+    int PlaneCrash;
+    int PlaneDepart;
+    int PlaneArrive;
+    int PPLSafeLanded;
+    int PPLKilled;
+    int GCKilled;
+    double AvgGCArrive;
+    double AvgGCDepart;
+    int SafeCargo;
+    int DestroyedCargo;
+    int TotalTime;
+
+};
+int main(){
+
+
+}
